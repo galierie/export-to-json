f7c6de13fae4474784dbc1b23c7186cfb329660c
Eriene Galinato <erienegalinato@gmail.com>
2025-03-18 19:48:15 +0800

    chore: set-up `eslint` config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,30 @@
-import config from "eslint-config-prettier";
+import eslintJS from '@eslint/js';
+import eslintJSON from '@eslint/json';
+import eslintPrettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...[].concat(config),
-];
+export default tsEslint.config(
+    {
+        files: ['**/*.js', '**/*.mjs'],
+        ...eslintJS.configs.recommended,
+    },
+    {
+        files: ['**/*.json'],
+        ignores: ['package.json'],
+        language: 'json/json',
+        ...eslintJSON.configs.recommended,
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
+    {
+        ignores: ['node_modules/**/*'],
+    },
+    eslintPrettierConfig,
+);
